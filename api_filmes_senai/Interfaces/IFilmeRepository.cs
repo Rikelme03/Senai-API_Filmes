ba71ec5738acf58b28261b753b07a320c6c01fa3
RIKELME MIRANDA SILVA <47540013893@info.local>
2025-03-11 13:26:40 -0300

    [200~feat: adicionando configuracoes de swegger, Cors Policy e sumario nos endpoints, implementando a autenticacao com JWT

---
+++ api_filmes_senai/Interfaces/IFilmeRepository.cs
@@ -0,0 +1,21 @@
+﻿using api_filmes_senai.Domains;
+
+namespace api_filmes_senai.Interfaces
+{
+    public interface IFilmeRepository
+    {
+        void Filme(Filme novoFilme);
+
+        List<Filme> Listar();
+
+        void Atualizar(Guid Id, Filme filme);
+
+        void Deletar(Guid Id);
+
+        Filme BuscarPorId(Guid Id);
+        void Cadastrar(Filme novoFilme);
+
+        List<Filme> listaDeFilmePorGenero(Guid idGenero);
+        List<Filme> ListarPorGenero(Guid id);
+    }
+}
